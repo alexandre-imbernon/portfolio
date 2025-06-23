f9a87da43330c8b34d464ec5b8adde075e5cab31
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2025-06-23 11:45:16 +0200

    clear div encapsuling the whole page now displayed

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
-import { Header } from './components/header';
+import { Header } from "./components/header";
 import { RetroBar } from "./components/RetroBar";
-import backgroundImage from './assets/bg.png';
-import { Footer } from "./components/footer"; // 👈 Important ici
-import { WindowsControls } from "./components/WindowsControl";
+import backgroundImage from "./assets/bg.png";
+import { Footer } from "./components/footer";
 
 export function App() {
-  const [currentSection, setCurrentSection] = useState('about');
+  const [currentSection, setCurrentSection] = useState("about");
 
   return (
-    <div className="relative min-h-screen">
+    <div className="fixed inset-0 border border-white/50">
+      {/* Arrière-plan principal avec image */}
       <div
-        className="bg-black min-h-screen bg-cover bg-center bg-no-repeat"
+        className="bg-black min-h-screen bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
+        {/* Composants principaux */}
         <Header />
         <RetroBar
           onSectionChange={setCurrentSection}
           currentSection={currentSection}
         />
         <Footer section={currentSection} />
+
+        {/* Cadres et effets visuels */}
+        <div className="fixed inset-0 border-x border-t border-white/50 z-50 pointer-events-none" />
+        <div className="fixed bottom-0 left-0 w-full h-[2px] bg-white/50 z-50 pointer-events-none" />
       </div>
     </div>
   );
